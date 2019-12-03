d549a39077d0294fecfebac9625ccf37c85b03fd
crysis <crysiswarhead020304050@gmail.com>
2019-12-03 17:02:08 +0200

    Changed calendar body

--- assets/view_model/createCalendarBody.js
+++ assets/view_model/createCalendarBody.js
@@ -2,15 +2,29 @@
 
 
 
+const calendarBody = document.createElement('tbody');
 
 
 
-export default function (month) {
 
-    const calendarBody = document.createElement('tbody');
-
-
-
-    return calendarBody;
+function createTr() {
+    const trElem = document.createElement("tr");
 }
+
+function createTd() {
+    const tdElem = document.createElement("td");
+}
+
+function createAllWeeks(month) {
+    month.weeks.forEach( week => {
+        const trWeek = createTr();
+        week.days.forEach( day => {
+           const tdDay = createTd();
+           tdDay.innerText = day.date();
+           trWeek.appendChild(tdDay);
+        });
+        calendarBody.appendChild(trWeek);
+    });
+}
+
 
